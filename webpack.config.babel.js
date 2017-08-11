44eb4efb2119c0c0d3dc38ab66b16189277c8934
BigWestGang <big.west.e.x.troopers@gmail.com>
2017-08-11 10:03:45 +0900

    1.Fix webpack setting

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,40 +1,39 @@
-import 'babel-polyfil'
-import path from 'path'
-import webpack from 'webpack'
+import 'babel-polyfill';
+import path from 'path';
+import webpack from 'webpack';
 
-const DEBUG = !process.argv.includes('--release')
-const VERBOSE = !process.argv.includes('--verbose')
+const DEBUG = !process.argv.includes('--release');
+const VERBOSE = !process.argv.includes('--verbose');
 
 export default {
   cache: DEBUG,
-  debug: DEBUG,
 
   stats: {
     colors: true,
-    reasons: DEBUG
+    reasons: DEBUG,
     hash: VERBOSE,
     version: VERBOSE,
     timings: true,
     chunks: VERBOSE
   },
 
-  entry: './src/app.js'
+  entry: './src/app.js',
   output: {
     publicPath: '/',
     sourcePrefix: '',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
     filename: 'app.js'
   },
   target: 'electron-main',
 
   devtool: DEBUG ? 'cheap-module-eval-sourcemap': false,
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx'],
   },
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, include: [path.resolve(__dirname, 'src')], loader: 'babel' },
+      { test: /\.jsx?$/, include: [path.resolve(__dirname, 'src')], loader: 'babel-loader' },
     ],
   },
 }
